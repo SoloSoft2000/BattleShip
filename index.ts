4e9262c55669dbdd0d9c7fbefffa098b03ba0703
Eugene Solomonyk <solosoft2000@mail.ru>
2024-02-14 16:01:35 +0200

    chore: typescript, eslint, prettier

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { gameServer } from "./src/gameServer";
+import { httpServer } from "./src/http_server/index";
+
+const HTTP_PORT = 8181;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
+
+gameServer.listen(3000, () => {
+  console.log('Server on Port 3000');
+})
