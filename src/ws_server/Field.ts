9b36db18dbc3ea228308ceae9c0d9c0079127b7f
Eugene Solomonyk <solosoft2000@mail.ru>
2024-02-18 16:43:34 +0200

    refactor: Field & Game

--- src/ws_server/Field.ts
+++ src/ws_server/Field.ts
@@ -6,32 +6,84 @@ interface Ship {
     x: number;
     y: number;
   };
+  hitLeft: number;
 }
 
+interface Cell {
+  ship: Ship | null;
+  hit: boolean;
+}
+
+export type ShotStatus = 'miss'|'killed'|'shot';
+
 export class Field {
-  private field: string[][];
-  hasShips = false;
+  private field: Cell[][];
+  private ships: Ship[];
 
   constructor(size: number = 10) {
-    this.field = Array.from({ length: size }, () => Array(size).fill(''));
+    this.field = Array.from( {length: size }, () => Array(size).fill({ship: null, hit: false}));
+    this.ships = [];
   }
 
-  placeShips(ships: Ship[]): void {
-    ships.forEach((ship) => {
-      const { x, y } = ship.position;
-      const direction = ship.direction ? 'vertical' : 'horizontal';
-      const length = ship.length;
+  placeShips(shipsForParce: Ship[]): void {
+    shipsForParce.forEach((item) => {
+      const { x, y } = item.position;
+      
+      const direction = item.direction ? 'vertical' : 'horizontal';
+      const length = Number(item.length);
 
       if (direction === 'horizontal') {
         for (let i = 0; i < length; i++) {
-          this.field[y][x + i] = ship.type;
+          this.field[y][x+i] = {
+            ship: item,
+            hit: false
+          };
         }
       } else {
         for (let i = 0; i < length; i++) {
-          this.field[y + i][x] = ship.type;
+          this.field[y+i][x] = {
+            ship: item,
+            hit: false
+          };
         }
       }
+
+      this.ships.push(item)
     });
-    this.hasShips = true;
+  }
+
+  printField(): string {
+    let result = '';
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        const shipInCell = this.field[x][y].ship;
+        result += shipInCell ? shipInCell.length.toString() : '0';
+      }
+      result += '\n';
+    }
+    return result;
+  }
+
+  getShips(): Ship[] {
+    return this.ships;
+  }
+
+  attack(x: number, y: number): ShotStatus {
+    const cell = this.field[y][x];
+    cell.hit = true;
+
+    let result = 'miss';
+    
+    if (cell.ship) {
+      cell.ship.hitLeft--;
+      if (cell.ship.hitLeft) {
+        result = 'shot';
+      } else {
+        result = 'killed';
+        this.ships = this.ships.filter(s => s !== cell.ship);
+      }
+    }
+
+    return result as ShotStatus;
   }
 }
--- src/ws_server/Game.ts
+++ src/ws_server/Game.ts
@@ -1,4 +1,4 @@
-import { Field } from './Field';
+import { Field, ShotStatus } from './Field';
 import { Player } from './Player';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -11,6 +11,8 @@ export class Game {
   private oponentFieldJSON: string = '';
   private ownerFieldJSON: string = '';
 
+  private turnId: number = 0;
+
   constructor(owner: Player, oponent: Player) {
     this.owner = owner;
     this.oponent = oponent;
@@ -19,6 +21,7 @@ export class Game {
     this.ownerField = new Field();
     this.oponentField = new Field();
 
+    
     let message: string = JSON.stringify({
       type: 'create_game',
       id: 0,
@@ -45,31 +48,84 @@ export class Game {
   handleMessage(message: string, isOwner: boolean): void {
     const { type, data } = JSON.parse(message.toString());
     if (type === 'add_ships') {
-      const { ships } = JSON.parse(data);
-      if (isOwner) {
-        this.ownerField.placeShips(ships);
-        this.ownerFieldJSON = ships;
-      } else {
-        this.oponentField.placeShips(ships);
-        this.oponentFieldJSON = ships;
-      }
-      if (this.ownerField.hasShips && this.oponentField.hasShips) {
-        let sendMessage = JSON.stringify({
-          type: 'start_game',
-          id: 0,
-          data: JSON.stringify({ ships: this.ownerFieldJSON }),
-          currentPlayerIndex: 0,
-        });
-        this.owner.getWS().send(sendMessage);
-
-        sendMessage = JSON.stringify({
-          type: 'start_game',
-          id: 0,
-          data: JSON.stringify({ ships: this.oponentFieldJSON }),
-          currentPlayerIndex: 1,
-        });
-        this.oponent.getWS().send(sendMessage);
-      }
+      this.addShips(data, isOwner);
     }
+    if (type === 'attack') {
+      this.attack(data);
+    }
+  }
+
+  addShips(data: string, isOwner: boolean): void {
+    const { ships } = JSON.parse(data);
+    if (isOwner) {
+      this.ownerField.placeShips(ships);
+      this.ownerFieldJSON = ships;
+      console.log(this.ownerField.printField());
+    } else {
+      this.oponentField.placeShips(ships);
+      this.oponentFieldJSON = ships;
+      console.log(this.oponentField.printField());
+    }
+    if (this.ownerField.getShips().length && this.oponentField.getShips().length) {
+      let sendMessage = JSON.stringify({
+        type: 'start_game',
+        id: 0,
+        data: JSON.stringify({ ships: this.ownerFieldJSON }),
+        currentPlayerIndex: this.owner.getId(),
+      });
+      this.owner.getWS().send(sendMessage);
+      this.turn(this.owner, this.owner.getId());
+
+      sendMessage = JSON.stringify({
+        type: 'start_game',
+        id: 0,
+        data: JSON.stringify({ ships: this.oponentFieldJSON }),
+        currentPlayerIndex: this.oponent.getId(),
+      });
+      this.oponent.getWS().send(sendMessage);
+      this.turn(this.oponent, this.owner.getId());
+    }
+  }
+
+  turn(player: Player, playIndex: number): void {
+    this.turnId = playIndex;
+    const message = JSON.stringify({
+      type: 'turn',
+      data: JSON.stringify({currentPlayer: playIndex})
+    })
+
+    player.getWS().send(message);
+  }
+
+  attack(data: string): void {
+    const { x, y, indexPlayer } = JSON.parse(data);
+    if (this.turnId !== indexPlayer) {
+      return;
+    }
+
+    const isOwner = this.owner.getId() === indexPlayer;
+    
+    const result = isOwner ? this.oponentField.attack(x, y) : this.ownerField.attack(x, y);
+    
+    this.feedback(indexPlayer, x, y, result);
+    if (result === 'miss') {
+      this.turn(this.owner, isOwner ? this.oponent.getId() : this.owner.getId());
+      this.turn(this.oponent, isOwner ? this.oponent.getId() : this.owner.getId());
+    }
+  }
+
+  feedback(currentPlayer: number, x: number, y: number, status: ShotStatus): void {
+    const data = { 
+      position: { x, y },
+      currentPlayer,
+      status
+    };
+    const message = {
+      type: 'attack',
+      data: JSON.stringify(data),
+      id: 0
+    }
+    this.owner.getWS().send(JSON.stringify(message));
+    this.oponent.getWS().send(JSON.stringify(message));
   }
 }
